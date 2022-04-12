44337579a2d2dbba4616003d9271da923b6b9cbd
Ezabulbary <ezabul.bary@gmail.com>
2022-04-12 11:54:31 +0600

    Six core concept for react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+function ExternalUsers(){
+  const [users, setUsers] = useState([]);
+  // useEffect( () => {}, []);
+  useEffect( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, []);
+  return(
+    <div>
+      <h2>External Users</h2>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return(
+    <div className="containar">
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
+
 
 function Counter(){
   const [count, setCount] = useState(0);
